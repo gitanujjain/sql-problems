d66c329583f51100df1754e1f25d12532f9dc4c3
Anuj Jain <47607397+gitanujjain@users.noreply.github.com>
2026-08-29 14:50:40 +0530

    29_08_26 2 sql problem

---
+++ leetcode/problem-177.sql
@@ -0,0 +1,34 @@
+-- 177. Nth Highest Salary
+-- Table: Employee
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | salary      | int  |
+-- +-------------+------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains information about the salary of an employee. 
+-- Write a solution to find the nth highest distinct salary from the Employee table. If there are less than n distinct salaries, return null.
+
+Create table If Not Exists Employee_177 (Id int, Salary int);
+Truncate table Employee_177;
+insert into Employee_177 (id, salary) values ('1', '100');
+insert into Employee_177 (id, salary) values ('2', '200');
+insert into Employee_177 (id, salary) values ('3', '300');
+insert into Employee_177 (id, salary) values ('4', '100');
+insert into Employee_177 (id, salary) values ('4', '200');
+
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+    select distinct emp.salary 
+    from (select Employee_177.salary, DENSE_RANK() over( order by Employee_177.salary Desc) as rank from Employee_177) as emp
+    where emp.rank = N
+    
+      
+  );
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT * FROM NthHighestSalary(2);
---
+++ leetcode/problem-196.sql
@@ -0,0 +1,31 @@
+-- 196. Delete Duplicate Emails
+-- Table: Person
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | email       | varchar |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+-- Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+
+-- For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
+
+Create table If Not Exists Person_196 (Id int, Email varchar(255));
+Truncate table Person_196;
+insert into Person_196 (id, email) values ('1', 'john@example.com');
+insert into Person_196 (id, email) values ('2', 'bob@example.com');
+insert into Person_196 (id, email) values ('3', 'john@example.com');
+
+select id, email, dense_rank() over (partition by email order by id) as rank
+from Person_196
+
+with delete_cte as (ssss
+	select id, 
+			email, 
+			dense_rank() over (partition by email order by id) as ran
+	from Person_196)
+delete FROM Person_196 where id in (select id from delete_cte  WHERE ran >1)
